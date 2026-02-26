9769f93447c110f4cb75eca2c16e40af1cab7335
Salim B <git@salim.space>
2026-02-26 14:57:30 +0100

    fix: omit empty els before concatenating

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ handlebars.registerHelper({
   concat: (...args) => args.filter(arg => typeof arg !== 'object').join(''),
   // Arguments: {address, city, subdivision, postalCode, countryCode}
   // formatAddress: (...args) => addressFormat(args).join(' '),
-  formatAddress: (...args) => args.filter(arg => typeof arg !== 'object').join('<br>'),
+  formatAddress: (...args) => args.filter(arg => typeof arg !== 'object').filter(Boolean).join('<br>'),
   formatDate: date => moment(date).format('MM/YYYY'),
   lowercase: s => s.toLowerCase(),
   eq: (a, b) => a === b,
